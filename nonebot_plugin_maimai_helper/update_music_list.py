f4e43d6a5aa14ccee10b4a77665bb7ca631b0d5a
YTSakura233 <callmefft@163.com>
2024-09-22 10:29:58 +0800

    更新：更新插件配置信息默认值，更改更新水鱼乐曲列表的方式。

--- nonebot_plugin_maimai_helper/update_music_list.py
+++ nonebot_plugin_maimai_helper/update_music_list.py
@@ -3,6 +3,7 @@ import os
 
 import requests
 from nonebot.log import logger
+from src.plugins.maimai_new.data import root_path
 
 
 URL = "https://www.diving-fish.com/api/maimaidxprober/music_data"
@@ -11,19 +12,12 @@ URL = "https://www.diving-fish.com/api/maimaidxprober/music_data"
 def update_music_list():
     logger.success("开始更新music_list")
     logger.success("开始删除文件")
-    if os.path.isfile('./game_data/diving_music_list.json'):
-        os.remove('./game_data/diving_music_list.json')
+    if os.path.isfile(root_path + '/game_data/diving_music_list.json'):
+        os.remove(root_path + '/game_data/diving_music_list.json')
     new_data = requests.get(URL)
     new_data.encoding = 'utf-8'
     data = json.loads(new_data.text)
     logger.success("开始更新文件")
-    with open('./game_data/diving_music_list.json', 'w', encoding='utf-8') as old_data:
+    with open(root_path + '/game_data/diving_music_list.json', 'w', encoding='utf-8') as old_data:
         json.dump(data, old_data, ensure_ascii=False, indent=4)
 
-
-if __name__ == '__main__':
-    try:
-        update_music_list()
-        logger.success("更新成功")
-    except Exception as e:
-        logger.error(f"更新失败：{e}")
